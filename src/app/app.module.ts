3f27477c99efb81146fe8987770dd3f4eec7bd94
Geetanjali <geetanjali.bgs@gmail.com>
2023-11-05 02:40:58 +0530

    Updated file structure , modified home page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SharedModule } from 'src/shared/shared.module';
+//ng g s shared
+import { MaterialModule } from 'src/material/material.module';
+//ng generate module material
 
 @NgModule({
   declarations: [AppComponent],
